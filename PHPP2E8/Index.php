71b0a8bf4cc1243a5ef43ab64b1149650e8672e0
Jérôme DE PORRES <62543727+jeromedeporres@users.noreply.github.com>
2020-06-09 16:32:18 +0200

    Update Index.php

--- PHPP2E8/Index.php
+++ PHPP2E8/Index.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
 <head>
-    <meta charset="utf-8"/>
+    <meta charset="utf-8" />
     <title>PHPP2EXO08</title>
 </head>
     <body>
         <p><?php
-             $isOk = 0;
-             if ($isOk == 1) 
+             $isOk = false;
+             if ($isOk) 
              {
                  echo 'c\'est ok !!';
              }
